1c342c935d75f0f8eb11231a745d043c5db7c1bd
Patryk Zachnik <patrykz.pz@gmail.com>
2021-10-04 14:14:00 +0200

    Files added
    ==

---
+++ FridayRem.py
@@ -0,0 +1,39 @@
+from ntpath import join
+import pandas as pd
+import os
+
+
+user_input = "Mu2.xlsx"
+user_input = os.path.join(os.path.dirname(os.path.abspath(__file__)), user_input)
+xls = pd.ExcelFile(user_input)
+df = pd.read_excel(xls,index_col=False,na_values=["NA"], engine="openpyxl")
+
+folder_Name = os.path.dirname(os.path.abspath(__file__))
+# Removed from list by client 
+def by_client():
+    client_file_path = os.path.join(folder_Name, "Clients.txt")
+    with open(client_file_path) as f:
+            client_list = [line.rstrip() for line in f]
+    for x in client_list:
+            df.drop(df[df["Client"]  == x].index,inplace=True)
+# ==================================
+# Removed from list by domain
+def by_domain():
+    client_file_path = os.path.join(folder_Name, "Domain.txt")
+    with open(client_file_path) as f:
+            client_list = [line.rstrip() for line in f]
+    for x in client_list:
+            df.drop(df[df["Domain"]  == x].index,inplace=True)
+#===================================
+# Removed from list by two statements
+# df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == "DOKER"].index ,inplace=True)
+# print(df["Name"]  == df['Name'].str.extract('(\$\.d*\.?\d*)')
+# df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == str.startswith("v")].index ,inplace=True)
+
+    # if i.lower().startswith("v"):
+        # df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == i],inplace=True)
+
+
+
+
+# print(df)
