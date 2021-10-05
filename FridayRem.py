b6f686fba4cde3cf731ee8dd40b9f868cba17e82
Patryk Zachnik <patrykz.pz@gmail.com>
2021-10-05 13:04:21 +0200

    Adding additional functions

--- FridayRem.py
+++ FridayRem.py
@@ -16,31 +16,56 @@ def by_client():
             client_list = [line.rstrip() for line in f]
     for x in client_list:
             df.drop(df[df["Client"]  == x].index,inplace=True)
-# ==================================
+# ===================================
 # Removed from list by domain
 def by_domain():
-    client_file_path = os.path.join(folder_Name, "Domain.txt")
-    with open(client_file_path) as f:
-            client_list = [line.rstrip() for line in f]
-    for x in client_list:
+    Domain_file_path = os.path.join(folder_Name, "Domain.txt")
+    with open(Domain_file_path) as f:
+            Domain_list = [line.rstrip() for line in f]
+    for x in Domain_list:
             df.drop(df[df["Domain"]  == x].index,inplace=True)
-#===================================
+# ===================================
 # Removed from list by two statements
-name_list = df["Name"].values.tolist()
-for i in name_list:
-        if i.startswith("V"):
-                df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == i].index ,inplace=True)
+def by_twostatements():
+        name_list = df["Name"].values.tolist()
+        for i in name_list:
+                if i.startswith("V"):
 
-        else:
-                pass
+                        df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == i].index ,inplace=True)
 
+                else:
 
-df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == "DOKER"].index ,inplace=True)
-
-    # if i.lower().startswith("v"):
-        # df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == i],inplace=True)
+                        continue
 
 
+        df.drop(df[df["Client"]  == "Client5"].index & df[df["Name"]  == "DOKER"].index ,inplace=True)
+# ===================================
+# Removed from list because it's France
+def by_france():
+        name_list = df["Name"].values.tolist()
+        for i in name_list:
+                if i.startswith("IFR") | i.startswith("PFR"): #Removed from list because it's France
 
+                                df.drop(df[df["Name"]  == i].index ,inplace=True) #Removed from list because it's France
+                else:
+                        
+                        continue
+
+        df.drop(df[df["IP"]  == "125.22.33"].index, inplace=True)
+
+# ===================================
+# Removed from list because Afee
+def by_Afee():  
+     Affe_file_path = os.path.join(folder_Name, "Afee.txt")
+     with open(Affe_file_path) as f:
+            Affe_list = [line.rstrip() for line in f]
+     for x in Affe_list:
+         df.drop(df[df["Name"]  == x].index ,inplace=True)
+# ===================================
+# Removed from list 
+# by_client()
+# by_domain()
+# # by_twostatements()
+by_Afee()
 
 print(df)
